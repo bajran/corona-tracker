6eac61cc7f588d9e5b6b8a5b996e40c55c213973
bajran <youraksh@gmail.com>
2020-04-12 13:37:16 +0530

    Updates

---
+++ precache-manifest.058d244bc09144f87230a4d2c86a7455.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252801ec893a69579780547b19d577bb",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "78c4033a542a9f1e5944",
+    "url": "/covid-tracker/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "f343b8d087158ea8c37c",
+    "url": "/covid-tracker/static/css/main.80776f12.chunk.css"
+  },
+  {
+    "revision": "78c4033a542a9f1e5944",
+    "url": "/covid-tracker/static/js/2.f600b90b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-tracker/static/js/2.f600b90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f343b8d087158ea8c37c",
+    "url": "/covid-tracker/static/js/main.9f86751c.chunk.js"
+  },
+  {
+    "revision": "9bdd3523f1d3242d80e0",
+    "url": "/covid-tracker/static/js/runtime-main.1fa2335b.js"
+  }
+]);
